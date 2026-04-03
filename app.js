31f6fe6921d60597ac6a4feb172c86a9477fa2ab
Ayush Saini <ayush.saini@infozech.com>
2026-04-03 20:20:27 +0530

    First React Code

---
+++ app.js
@@ -0,0 +1,21 @@
+// document.getElementById('root').innerHTML ="Hello";
+
+{/* <div id = "parent">
+  <div id = "child">
+    <h1>I'm h1 tag</h1>
+    <h2>I'm h2 tag</h2>
+</div>
+</div> */}
+
+
+const parent = React.createElement("div" , {id: "parent"},
+  React.createElement("div", {id:"child"}, 
+    [React.createElement("h1", {} , "I'm h1 tag"),
+      React.createElement("h1", {} , "I'm h1 tag")]
+  )
+);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
