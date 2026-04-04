d1377f13a0c8a56ee0493985d107c358a94d5f9b
ayushsaini16 <sainiayush064@gmail.com>
2026-04-04 11:18:45 +0530

    Bundler(Parcel)

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-// document.getElementById('root').innerHTML ="Hello";
-
-{/* <div id = "parent">
-  <div id = "child">
-    <h1>I'm h1 tag</h1>
-    <h2>I'm h2 tag</h2>
-</div>
-</div> */}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
 const parent = React.createElement("div" , {id: "parent"},
