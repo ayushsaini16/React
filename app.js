5e29305cb52e841632f19945843fa0098d3fea46
ayushsaini16 <sainiayush064@gmail.com>
2026-04-04 23:28:37 +0530

    JSX, React Functional Component

--- app.js
+++ app.js
@@ -2,14 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const parent = React.createElement("div" , {id: "parent"},
-  React.createElement("div", {id:"child"}, 
-    [React.createElement("h1", {} , "I'm h1 tag"),
-      React.createElement("h1", {} , "I'm h1 tag")]
-  )
-);
+// const parent = React.createElement("div" , {id: "parent"},
+//   React.createElement("div", {id:"child"}, 
+//     [React.createElement("h1", {} , "I'm h1 tag"),
+//       React.createElement("h1", {} , "I'm h1 tag")]
+//   )
+// );
 
-console.log(parent);
+//JSX
+// const jsxHeading = (
+//   <div>
+// <h1 className = "heading">This is React</h1>
+//   <h2 className = "heading">This is ReactDOM</h2>
+//   </div>
+// );
+
+const JsxHeading = () => (
+  <div>
+<h1 className = "heading">This is React</h1>
+  <h2 className = "heading">This is ReactDOM</h2>
+  </div>
+);
+const ele = 1000;
+
+
+//React Functional Component
+const HeadingComponent = () => { return (
+  <div>
+    {ele}
+    <JsxHeading />
+    <h1>This is a React Component</h1>
+</div>)};
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
